ecc8f5fe7b1fad1339f65a95d0dafd597973d3b3
Alan Araujo Leite <alansl@outlook.com>
2021-11-09 19:19:01 -0300

    Continuação dos estudos

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40;
+console.log(`A velocidade do seu carro é ${vel}kmt/h`);
+if (vel > 50){
+    console.log(`Você ultrapassou a velocidade limite. MULTADO!`);
+}
+console.log(`Dirija com segurança`);
---
+++ cursojs/aula11/ex009.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil';
+console.log(`Vivendo em ${pais} você é `);
+if (pais == 'Brasil'){
+    console.log('brasileiro');
+} else {
+    console.log('estrangeiro');
+}
